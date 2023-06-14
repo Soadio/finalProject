08e685ac28962df73421fdb504c0c3bf1eb0131c
Sheriff Adio <flamezee15@gmail.com>
2023-06-14 20:48:43 +0100

    first commit

---
+++ src/components/Header.jsx
@@ -0,0 +1,9 @@
+import Button from "./Button";
+export default function Header() {
+  return (
+    <header className="header">
+      <p>Mortgage Pre-Assessment</p>
+      <Button>Sign up</Button>
+    </header>
+  );
+}
